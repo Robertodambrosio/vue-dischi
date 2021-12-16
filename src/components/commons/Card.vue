49bc51adb785c5fbe6ac0c68d4caeea19d15c549
Roberto D'Ambrosio <roberto8601@gmail.com>
2021-12-16 16:18:49 +0100

    aggiunta hover img

--- src/components/commons/Card.vue
+++ src/components/commons/Card.vue
@@ -20,14 +20,22 @@ props: {
     .card-song {
         max-width: calc(20% - 20px);
         min-height: 350px;
-        margin-bottom: 20px;
         text-align: center;
+        margin-top: 20px;
         background-color: #2e3a46; 
         padding: 15px; 
         
         img{
             width: 100%;
             margin-bottom: 20px;
+            filter: grayscale(100%);
+            transition: all 0.3s ease-in-out;
+            cursor: pointer;
+
+            &:hover {
+                filter: grayscale(0%);
+                transform: scale(1.1);
+            }
         }
 
         h4 {
