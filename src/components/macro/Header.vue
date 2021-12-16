b7e2587c84b296698e961e3c3081d80e3f7e94e1
Roberto D'Ambrosio <roberto8601@gmail.com>
2021-12-16 14:41:55 +0100

    header

--- src/components/macro/Header.vue
+++ src/components/macro/Header.vue
@@ -1,5 +1,8 @@
 <template>
   <header>
+      <div class="container">
+        <img src="../../assets/img/spotify-logo.png" alt="logo-spotify">
+      </div>
   </header>
 </template>
 
@@ -10,5 +13,22 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+
+header {
+    height: 80px;
+    background: #2e3a46;
+    
+    .container {
+        width: 95%;
+        height: 100%;
+        margin: 0 auto;
+        display: flex;
+        align-items: center;
+
+        img {
+            width: 50px;
+        }
+    }
+}
 
 </style>
